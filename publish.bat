112d2bd373b8c8378dc6fa90ab910913976ca4d3
Jon Ander <jon-1200@hotmail.es>
2023-10-30 23:29:49 +0100

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -9,8 +9,7 @@ set "publish_gma=workshop.gma"
 set "publish_id=3066799276"
 
 call %gmad% create -folder %publish_path%
-call %gmpublish% update -addon %publish_gma% -id %publish_id% -changes "Forked cinema for testing purposes (Trying to fix Yugenanime) see project in: https://github.com/Jaie55/cinema_test/tree/master
-- Added the possibility to bypass file extention check with a convar"
+call %gmpublish% update -addon %publish_gma% -id %publish_id% -changes "Forked cinema for testing purposes (Trying to fix Yugenanime) see project in: https://github.com/Jaie55/cinema_test/tree/master Added the possibility to bypass file extention check with a convar"
 del %publish_gma%
 
 
