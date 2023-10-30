3df54307c357087fdf71ae07f1e8ef93b5f78fb5
Jon Ander <jon-1200@hotmail.es>
2023-10-30 21:59:44 +0100

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -6,7 +6,7 @@ set "gmpublish=%basepath%\gmpublish.exe"
 
 set "publish_path=workshop"
 set "publish_gma=workshop.gma"
-set "publish_id=2419005587"
+set "publish_id=3066799276"
 
 call %gmad% create -folder %publish_path%
 call %gmpublish% update -addon %publish_gma% -id %publish_id% -changes "See https://github.com/PurrCode/cinema"
